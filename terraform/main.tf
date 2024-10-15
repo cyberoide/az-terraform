d6cff5a10c74710226eb4d8981a972cebd068532
cyberoide tech <145835084+cyberoide@users.noreply.github.com>
2024-10-15 18:46:34 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,13 +4,13 @@ provider "azurerm" {
 
 # Resource Group
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "example-resources-test"
   location = "East US"
 }
 
 # Virtual Network
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+  name                = "example-network-test"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
@@ -18,7 +18,7 @@ resource "azurerm_virtual_network" "example" {
 
 # Subnet
 resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
+  name                 = "example-subnet-test"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
@@ -26,7 +26,7 @@ resource "azurerm_subnet" "example" {
 
 # Network Interface
 resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
+  name                = "example-nic-test"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
